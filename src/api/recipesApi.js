5d02586015a61ce82c5fd1c176efe35df799705a
Scott Gemmell <scott.gemmell@mac.com>
2021-08-27 23:41:09 +0100

    PATCH

--- src/api/recipesApi.js
+++ src/api/recipesApi.js
@@ -10,10 +10,43 @@ export const recipesApi = createApi({
 		getRecipeById: builder.query({
 			query: (id) => `recipes/${id}`,
 		}),
+		// postRecipeById: builder.query({
+		// 	query: (id) => `recipes/${id}`,
+		// }),
+		updateRecipeById: builder.mutation({
+			// note: an optional `queryFn` may be used in place of `query`
+			query: ({ id, ...patch }) => ({
+			  url: `recipes/${id}`,
+			  method: "PATCH",
+			  body: patch,
+			}),
+			// Pick out data and prevent nested properties in a hook or selector
+			transformResponse: (response) => response,
+			// invalidatesTags: ['Post'],
+			// onQueryStarted is useful for optimistic updates
+			// The 2nd parameter is the destructured `MutationLifecycleApi`
+			// async onQueryStarted(
+			//   arg,
+			//   { dispatch, getState, queryFulfilled, requestId, extra, getCacheEntry }
+			// ) {},
+			// The 2nd parameter is the destructured `MutationCacheLifecycleApi`
+			// async onCacheEntryAdded(
+			//   arg,
+			//   {
+			// 	dispatch,
+			// 	getState,
+			// 	extra,
+			// 	requestId,
+			// 	cacheEntryRemoved,
+			// 	cacheDataLoaded,
+			// 	getCacheEntry,
+			//   }
+			// ) {},
+		})
 		// getPokemonByName: builder.query({
 		// 	query: (name) => `pokemon/${name}`,
 		// }),
 	}),
 });
 
-export const { useGetRecipesQuery, useGetRecipeByIdQuery } = recipesApi;
+export const { useGetRecipesQuery, useGetRecipeByIdQuery, useUpdateRecipeByIdMutation } = recipesApi;
--- src/pages/PageEdit/hooks.js
+++ src/pages/PageEdit/hooks.js
@@ -1,17 +1,19 @@
 import { useState } from "react"
 import { useHistory, useParams } from "react-router-dom";
-import { useGetRecipeByIdQuery } from "../../api/recipesApi";
+import { useGetRecipeByIdQuery, useUpdateRecipeByIdMutation } from "../../api/recipesApi";
 
 export const usePageEdit = () => {
 
 	const params = useParams();
 	const { data: { title, slug, img }  } = useGetRecipeByIdQuery(+params.id);
+
+	const [updatePost] = useUpdateRecipeByIdMutation(+params.id)
 	
 	let [editedTitle, setEditedTitle] = useState(title);
 	let [editedSlug, setEditedSlug] = useState(slug);
 	let [editedImg, setEditedImg] = useState(img);
 
-	const history = useHistory();
+	//const history = useHistory();
 	
 	// useEffect(() => {
 	// 	console.log('editedTitle', { editedTitle });
@@ -24,6 +26,8 @@ export const usePageEdit = () => {
 	const handleSubmit = e => {
 		e.preventDefault();
 		console.log("SUBMIT");
+
+		updatePost({id: +params.id, title: editedTitle, slug: editedSlug, img: editedImg})
 
 		// const updatedRecipes = storedRecipes.map(recipe => +recipe.id === params.id 
 		// 	? {...recipe, title: editedTitle, slug: editedSlug, img: editedImg }
