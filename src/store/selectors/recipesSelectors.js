25d2e5ca1453b3839829d547b44942212fc46f0d
Scott Gemmell <scott.gemmell@mac.com>
2021-08-27 19:22:52 +0100

    removed service worker, logo.svg

--- src/store/selectors/recipesSelectors.js
+++ src/store/selectors/recipesSelectors.js
@@ -1,3 +1,5 @@
+// import { createSelector } from "@reduxjs/toolkit";
+
 export const selectRecipes = state => state.recipes;
 
 export const selectRecipe = state => state.recipes.list.find(recipe => +recipe.id === 1);
